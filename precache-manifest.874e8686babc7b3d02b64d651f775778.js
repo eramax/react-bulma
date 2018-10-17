351d87903ed34e2e7e264fd0d6536487dfe6124c
Ahmed Essam <eng.eramax@gmail.com>
2018-10-17 23:39:05 +0300

    Updates

---
+++ precache-manifest.874e8686babc7b3d02b64d651f775778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b623868c856af067b306",
+    "url": "/react-bulma/static/js/runtime~main.b623868c.js"
+  },
+  {
+    "revision": "461c46d567d50056948f",
+    "url": "/react-bulma/static/js/main.461c46d5.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-bulma/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "461c46d567d50056948f",
+    "url": "/react-bulma/static/css/main.5653b574.chunk.css"
+  },
+  {
+    "revision": "1b0d165b33d0a9e1bf884b92135b112d",
+    "url": "/react-bulma/index.html"
+  }
+];
